17ea35b4d760a13263a72c31df49485483470965
Shivom-S <149164292+Shivom-S@users.noreply.github.com>
2024-09-25 16:40:41 +0530

    a1-5.cpp

--- a1-5.cpp
+++ a1-5.cpp
@@ -24,11 +24,21 @@ class Stack {
         // Function to add an element to the stack
         void push(int x) {
             //Write your code here
+            if (top == capacity - 1) {
+            cout << "Stack Overflow" << endl;
+            return;
+        }
+        arr[++top] = x;
         }
 
         // Function to pop the top element
         int pop() {
             //Write your code here
+            if (top == -1) {
+            cout << "Stack Underflow" << endl;
+            return -1;
+        }
+        return arr[top--];
         }
 
         // Function to display the elements of the stack
@@ -48,11 +58,13 @@ class Stack {
         // Function to check if the stack is full
         bool isFull() const {
             //Write your code here
+            return top == capacity - 1;
         }
 
         // Function to check if the stack is empty
         bool isEmpty() const {
             //Write your code here
+            return top == -1;
         }
 
 };
@@ -75,6 +87,17 @@ void displayTowers() {
 
 void rearrangeDisks(int n, Stack& A, Stack& B, Stack& C, char from, char to, char aux) {
     //Write your code here
+    if (n == 1) {
+        cout << "Move disk 1 from " << from << " to " << to << endl;
+        C.push(A.pop());
+        displayTowers();
+        return;
+    }
+    rearrangeDisks(n - 1, A, C, B, from, aux, to);
+    cout << "Move disk " << n << " from " << from << " to " << to << endl;
+    C.push(A.pop());
+    displayTowers();
+    rearrangeDisks(n - 1, B, A, C, aux, to, from);
 }
 
 int main() { // The main function has been defined for you, do not edit anything here.
